fd03814e756a2ad7a3d917f87aaa2a4da6daba96
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-10-30 19:43:29 -0300

    subidas vistas en el servidor

--- servidor/index.js
+++ servidor/index.js
@@ -3,6 +3,7 @@ const app = express();
 const puerto = 3000;
 const mongoose = require("mongoose");
 const rutas = require("./rutas/rutas.js");
+const path = require("path");
 const rutasPublicaciones = require("./rutas/publicaciones.js");
 
 app.use(express.json());
@@ -14,7 +15,13 @@ mongoose
   .then(() => console.log("Conexion Exitosa"));
 //Conexion DB
 
-app.use("/", rutas);
+// app.use("/", rutas);
+app.use(express.static(path.join(__dirname, "public")));
+const htmlPath = path.join(__dirname, "public", "index.html");
+
+app.get("/", (req, res) => {
+  res.sendFile(htmlPath);
+});
 
 app.use("/publicaciones", rutasPublicaciones);
 
