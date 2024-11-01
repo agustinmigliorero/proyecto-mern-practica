504b6ac5a9350433e7c2a10fbd90ddf66a8b07fd
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-01 19:32:42 -0300

    borrado publicaciones1

--- servidor/modelos/publicacion1.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const publicacionSchema = new Schema({
-  titulo: { type: String },
-  texto: { type: String },
-});
-
-module.exports = mongoose.model("Publicacion1", publicacionSchema);
